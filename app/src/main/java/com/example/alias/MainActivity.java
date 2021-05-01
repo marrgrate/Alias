2a6a2a38b0e9bb4b56fb936932a0c32fcb6a470d
marrgrate <marrygran28@gmail.com>
2021-05-01 19:06:48 +0300

    processing button clicks using R.id

--- app/src/main/java/com/example/alias/MainActivity.java
+++ app/src/main/java/com/example/alias/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.alias;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View view){
-        Intent intent = new Intent(this, ModeMenuActivity.class);
-        startActivity(intent);
-    }
-    public void onExitClicked(View view){
-       finish();
-       //return();
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_start: {
+                Intent intent = new Intent(this, ModeMenuActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.button_rules: {
+                Intent intent = new Intent(this, RulesActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.button_exit: {
+                System.exit(0);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/alias/ModeMenuActivity.java
+++ app/src/main/java/com/example/alias/ModeMenuActivity.java
@@ -12,10 +12,24 @@ public class ModeMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mode_menu);
     }
     public void onOfflineClicked(View view){
-        Intent intent = new Intent(this, SettingsActivity.class);
+        Intent intent = new Intent(this, TeamsActivity.class);
         startActivity(intent);
     }
     public void onCancelClicked(View view){
         finish();
+    }
+
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_offline: {
+                Intent intent = new Intent(this, TeamsActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.button_network: {
+
+                break;
+            }
+        }
     }
 }
