4c6b7d5c5df172fac07ad6aa2765bbbd381f087d
marrgrate <marrygran28@gmail.com>
2021-05-01 17:36:10 +0300

    rules action added

---
+++ app/src/main/java/com/example/alias/RulesActivity.java
@@ -0,0 +1,17 @@
+package com.example.alias;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class RulesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rules_activity);
+    }
+}
