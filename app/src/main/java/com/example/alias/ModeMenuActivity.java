5fada86a286e2b7f58bf93b29bdb0fffdcf88181
marrgrate <marrygran28@gmail.com>
2021-05-02 17:15:00 +0300

    action bar wt back button added

--- app/src/main/java/com/example/alias/ModeMenuActivity.java
+++ app/src/main/java/com/example/alias/ModeMenuActivity.java
@@ -4,22 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ModeMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_menu);
-    }
-    public void onOfflineClicked(View view){
-        Intent intent = new Intent(this, TeamsActivity.class);
-        startActivity(intent);
-    }
-    public void onCancelClicked(View view){
-        finish();
+
+        Toolbar toolbar = findViewById(R.id.mode_action_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void onButtonClick(View view) {
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_offline: {
                 Intent intent = new Intent(this, TeamsActivity.class);
@@ -27,7 +25,6 @@ public class ModeMenuActivity extends AppCompatActivity {
                 break;
             }
             case R.id.button_network: {
-
                 break;
             }
         }
