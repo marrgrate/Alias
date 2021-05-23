428960c6b2ddc0b570c82a4b085e684cdd8c587d
marrgrate <marrygran28@gmail.com>
2021-05-24 01:52:36 +0300

    added Result model methods

--- app/src/main/java/com/example/alias/offline/model/Result.java
+++ app/src/main/java/com/example/alias/offline/model/Result.java
@@ -1,20 +1,55 @@
 package com.example.alias.offline.model;
 
-public class Result {
+import java.io.Serializable;
+
+public class Result implements Serializable {
     private static final int SKIPPED = 0;
     private static final int GUESSED = 1;
     private static final int GUESSED_BY_OTHER_TEAM = 2;
 
     protected int result;
-    String word;
+    private int roundResult;
+    private int guessed;
+    private int skipped;
 
-    public Result(String word, int res){
-        this.word = word;
-        this.result = res;
+    public Result() {
+        result = 0;
+        roundResult = 0;
+        guessed = 0;
+        skipped = 0;
     }
 
     public int getResult() {
         return result;
     }
-    //todo написать механизм подсчета слов
+
+    public void addPoint(){
+        roundResult++;
+        guessed++;
+    }
+
+    public void subtractPoint(){
+        if(roundResult>=1) {
+            roundResult--;
+            skipped++;
+        }
+    }
+
+    public void recountResult(){
+        result += roundResult;
+    }
+
+    public void resetRoundResult(){
+        roundResult = 0;
+        skipped = 0;
+        guessed = 0;
+    }
+
+    public int getSkipped() {
+        return skipped;
+    }
+
+    public int getGuessed() {
+        return guessed;
+    }
 }
