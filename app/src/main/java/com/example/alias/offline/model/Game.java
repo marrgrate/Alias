42fbf6a51bc7eff4777a90b1254c52e9bbb86055
marrgrate <marrygran28@gmail.com>
2021-05-24 01:48:58 +0300

    added setters and getters for Game

--- app/src/main/java/com/example/alias/offline/model/Game.java
+++ app/src/main/java/com/example/alias/offline/model/Game.java
@@ -2,56 +2,65 @@ package com.example.alias.offline.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 
 public class Game
         implements Serializable {
+   private static final int DEFAULT_MAX_TIME = 120;
+   private static final int DEFAULT_MAX_WORDS = 30;
+
    public ArrayList<Team> teams = new ArrayList<>();
    private Dictionary dictionarySelected;
-   private final String[] default_names = new String[]{"Хмарачосы", "Пацукі", "Парсюкі", "Валошкі", "Крумкачы"};
    private int maxTime = 60; //seconds
    private int maxWordsNumber = 60;    //max number of words to win
-   public Team teamPlaying = new Team();
+   private Team teamPlaying = null;
 
    public Game() {
       if(teams.size() == 0) {
-         addTeam();
-         addTeam();
-         setTeamPlaying(teams.get(1));
-      }
-      setMaxTime(60);
-      setMaxWordsNumber(60);
-   }
-
-   public void addTeam(){   //default name
-      if(teams.size()<=8) {
-         Random random = new Random();
-         Team team = new Team(default_names[random.nextInt(default_names.length)]);
+         teams.add(new Team());
+         Team team = new Team();
+         while(team.getName().equals(teams.get(0).getName())) {
+               team.setRandomName();
+            }
          teams.add(team);
+         setTeamPlaying();
       }
-   }
-   protected void setTeamPlaying(Team team){
-      teamPlaying.setName(team);
-   }
-
-   public void deleteTeam(){
-      if(teams.size()>2) {
-         teams.remove(teams.size() - 1);
-      }
+      setMaxTime(DEFAULT_MAX_TIME);
+      setMaxWordsNumber(DEFAULT_MAX_WORDS);
    }
 
-   public String[] getTeamsNames(){
-      String[] teamNames = new String[teams.size()];
-      for(int i = 0; i < teams.size(); i++) {
-         teamNames[i] = this.teams.get(i).getName();
+   public void setDictionarySelected(Dictionary dictionarySelected) {
+      this.dictionarySelected = dictionarySelected;
+   }
+
+   public Dictionary getDictionarySelected() {
+      return dictionarySelected;
+   }
+
+   public ArrayList<Team> getTeams() {
+      return teams;
+   }
+
+   public void setTeamPlaying(){
+      if(teamPlaying == null) {
+         teamPlaying = teams.get(0);
+         return;
       }
-      System.out.println(Arrays.toString(teamNames));
-      return teamNames;
+      if(teams.indexOf(teamPlaying) < teams.size() - 1)
+         teamPlaying.setTeam(teams.get(teams.indexOf(teamPlaying) + 1));
+      else
+         teamPlaying = teams.get(teams.size() - 1);
+   }
+
+   public Team getTeamPlaying() {
+      return teamPlaying;
    }
 
    public void setMaxTime(int maxTime) {
       this.maxTime = maxTime;
+   }
+
+   public int getMaxTime() {
+      return maxTime;
    }
 
    public void setMaxWordsNumber(int maxWordsNumber) {
@@ -59,20 +68,7 @@ public class Game
    }
 
    public void setDictionarySelected(String name){
-      dictionarySelected.name = name;
       //todo проинициализировать все поля выбранным словарем из джейсона
-   }
-
-   public ArrayList<Team> getTeams() {
-      return teams;
-   }
-
-   public String[] toStringList(ArrayList<Dictionary> list) {
-      String[] stringList = new String[list.size()];
-      for (int i = 0; i < teams.size(); i++) {
-         stringList[i] = list.get(i).name;
-      }
-      return stringList;
    }
 
    public int getMaxWordsNumber() {
