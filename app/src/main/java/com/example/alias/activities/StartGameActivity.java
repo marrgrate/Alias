400ecb2aa6fac04d7def637f1f28a68fac15a0c3
marrgrate <marrygran28@gmail.com>
2021-05-24 01:54:02 +0300

    replaced onClick to setOnClickListener

--- app/src/main/java/com/example/alias/activities/StartGameActivity.java
+++ app/src/main/java/com/example/alias/activities/StartGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,56 +12,80 @@ import com.example.alias.R;
 import com.example.alias.offline.model.Game;
 
 public class StartGameActivity extends AppCompatActivity {
-    Game myGame;
+    Game game;
     TextView timer;
     TextView teamPlays;
+    Button buttonStart;
     ImageButton skipButton;
     ImageButton acceptButton;
     TextView word;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
-        if (savedInstanceState != null) {
-            myGame = savedInstanceState.getParcelable("game");
-        }
+        Intent intent = getIntent();
+        game = (Game) intent.getSerializableExtra("game");
 
         skipButton = findViewById(R.id.button_skip);
         acceptButton= findViewById(R.id.button_accept);
+        buttonStart = findViewById(R.id.start_game);
         teamPlays = findViewById(R.id.text_view_team_name);
-        //todo определить команду которая играет
-        teamPlays.setText(myGame.teamPlaying.toString());
+        word = findViewById(R.id.word);
+
+        teamPlays.setText(game.getTeamPlaying().getName());
         skipButton.setVisibility(View.INVISIBLE);
         acceptButton.setVisibility(View.INVISIBLE);
 
-        word = findViewById(R.id.word);
-    }
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
+                timer = findViewById(R.id.timer);
+                new CountDownTimer(game.getMaxTime()* 1000L, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        timer.setText((millisUntilFinished/1000) < 10?
+                                "00:0"+ millisUntilFinished / 1000 :
+                                "00:"+ millisUntilFinished / 1000);
+                    }
 
-    public void onClick(View view){
-        if(view.getId() == R.id.start_game){
-            view.setVisibility(View.INVISIBLE);
-            timer = findViewById(R.id.timer);
-            new CountDownTimer(60000, 1000) {
-
-                public void onTick(long millisUntilFinished) {
-                    timer.setText((millisUntilFinished/1000) < 10?
-                            "00:0"+ millisUntilFinished / 1000 :
-                            "00:"+ millisUntilFinished / 1000);
+                    public void onFinish() {
+                        Intent intent;
+                        if(game.getTeamPlaying().getResult().getResult() < game.getMaxWordsNumber())
+                        {
+                            intent = new Intent(StartGameActivity.this, ResultActivity.class);
+                            game.getTeamPlaying().getResult().recountResult();
+                        } else {
+                            intent = new Intent(StartGameActivity.this, FinishGameActivity.class);
+                        }
+                        intent.putExtra("game", game);
+                        startActivity(intent);
+                        finish();
+                    }
                 }
-
-                public void onFinish() {
-                    Intent intent = new Intent(StartGameActivity.this, ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+                        .start();
+                skipButton.setVisibility(View.VISIBLE);
+                acceptButton.setVisibility(View.VISIBLE);
             }
-                    .start();
+        });
 
-            skipButton.setVisibility(View.VISIBLE);
-            acceptButton.setVisibility(View.VISIBLE);
-        } else if(view.getId()==R.id.button_accept) {
-            word.setText("new word accept");
-        } else if(view.getId()==R.id.button_skip) {
-            word.setText("new word skipped");
-        }
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.getTeamPlaying().result.addPoint();
+                String w = game.getDictionarySelected().getRandomWord();
+                game.getDictionarySelected().addUsedWord(w);
+                word.setText(w);
+            }
+        });
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.getTeamPlaying().result.subtractPoint();
+                String w = game.getDictionarySelected().getRandomWord();
+                game.getDictionarySelected().addUsedWord(w);
+                word.setText(w);
+            }
+        });
     }
 }
