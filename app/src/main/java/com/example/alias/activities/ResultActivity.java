11ebad677430a03522eadf13f95947569e024e67
marrgrate <marrygran28@gmail.com>
2021-05-24 01:53:08 +0300

    added connection to Result model

--- app/src/main/java/com/example/alias/activities/ResultActivity.java
+++ app/src/main/java/com/example/alias/activities/ResultActivity.java
@@ -1,9 +1,11 @@
 package com.example.alias.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.example.alias.R;
@@ -11,36 +13,52 @@ import com.example.alias.offline.model.Game;
 
 public class ResultActivity extends AppCompatActivity {
     Game game;
+    TextView guessedWords;
+    TextView skippedWords;
+    TextView general;
     Button nextButton;
+    ListView usedWords;
+
+    @SuppressLint("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        if (savedInstanceState != null) {
-            game = savedInstanceState.getParcelable("game");
-        }
+        Intent intent = getIntent();
+        game = (Game)intent.getSerializableExtra("game");
 
         Toolbar toolbar = findViewById(R.id.result_action_bar);
         setSupportActionBar(toolbar);
 
+        guessedWords = findViewById(R.id.text_view_guessed_num);
+        skippedWords = findViewById(R.id.text_view_skipped_num);
+        general = findViewById(R.id.text_view_general_result);
+        usedWords = findViewById(R.id.passed_words_list);
+
+        guessedWords.setText(game.getTeamPlaying().getResult().getGuessed()+"");
+        skippedWords.setText(game.getTeamPlaying().getResult().getSkipped()+"");
+        general.setText(game.getTeamPlaying().getResult().getResult()+"");
+
+        //todo вывод использованных слов
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                game.getUsedWords());
+//        usedWords.setAdapter(adapter);
+
         nextButton = findViewById(R.id.button_next_result);
-        nextButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent;
-                if(game.teamPlaying.result.getResult() <= game.getMaxWordsNumber()) {
-                    //todo запуск startGameActivity для следующей команды
-                    intent = new Intent(ResultActivity.this, StartGameActivity.class);
-                    intent.putExtra("game", game);
-                } else {
-                    intent = new Intent(ResultActivity.this, FinishGameActivity.class);
-                    intent.putExtra("winner", game.teamPlaying);
-                }
-                startActivity(intent);
-
+        nextButton.setOnClickListener(v -> {
+            Intent intent1;
+            if(game.getTeamPlaying().result.getResult() <= game.getMaxWordsNumber()) {
+                intent1 = new Intent(ResultActivity.this, StartGameActivity.class);
+                game.getTeamPlaying().getResult().resetRoundResult();
+                game.setTeamPlaying();
+                intent1.putExtra("game", game);
+                //todo startActivityForResult
+            } else {
+                intent1 = new Intent(ResultActivity.this, FinishGameActivity.class);
+                intent1.putExtra("winner", game.getTeamPlaying());
             }
+            startActivity(intent1);
         }
-        //todo если командой достигнуто максимальное количество очков - создать FinishGameActivity
         );
-
     }
 }
